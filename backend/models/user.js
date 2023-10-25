0f03c2dfc3b21caf4a70eabab4d1195672fe9756
Michael <mabilawon@gmail.com>
2023-10-25 18:15:37 +0100

    change in the users model

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,7 +6,6 @@ const UserSchema = new mongoose.Schema({
   lastname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, min: 6 },
-  // user_id: [{ type: Schema.Types.ObjectId, ref: "users" }],
 });
 
 module.exports = mongoose.model("User", UserSchema);
